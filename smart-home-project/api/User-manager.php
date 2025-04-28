cc354cfaf5cf9f9b17eb66defcb8c7084e6b8850
SABRI4 <younesysabri53@gmail.com>
2025-04-28 09:05:14 +0200

    header user_manager

--- smart-home-project/api/User-manager.php
+++ smart-home-project/api/User-manager.php
@@ -5,6 +5,12 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
+header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: http://localhost:3002");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
 require_once 'connect.php';
 
 $method = $_SERVER['REQUEST_METHOD'];
